20af4f208ab289f7f605fcd422d24aa608aef3a3
fellow-Git-user <martynas.nadeika@gmail.com>
2024-12-10 16:58:56 +0200

    Arrays first exercise with set

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+// let numbsArray = [2 , 3, 4];
+// numbsArray.unshift(1);
+// console.log(numbsArray);
+
+//Pirmas budas su new Set
+// let numbsArray = [1, 2, 2, 3, 3, 3, 4];
+// let noDuplicates = new Set(numbsArray);
+
+// let numbsArrayNoDuplicates = [...noDuplicates];
+
+// for (let i = 0; i < numbsArray.length; i++){
+    
+// }
+
+// for (let i = 0; i < numbsArrayNoDuplicates.length; i++){
+
+// }
+
+// let arrLength = numbsArrayNoDuplicates.length;
+
+// console.log(`[${numbsArray}] -> ${arrLength} (skirtingi elementai: ${numbsArrayNoDuplicates})`)
+
+
+
